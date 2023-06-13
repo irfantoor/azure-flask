0d082181bc7d7eb6cc1b33665dc90b0b8c115735
Irfan TOOR <email@irfantoor.com>
2023-06-13 14:26:41 +0200

    Azure-fn with flask

--- flask/__init__.py
+++ flask/__init__.py
@@ -1,24 +1,39 @@
+import os
+import json
 import logging
 
 import azure.functions as func
+from flask import Flask, app, request
 
+app = Flask(__name__)
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+scriptpath = os.path.abspath(__file__)
+ROOT = os.path.dirname(scriptpath)
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    return "I'm Home!"
+
+@app.route("/hello", methods=['POST', 'GET'])
+@app.route("/hello/<name>", methods=['GET'])
+def article(name:int=None):
+    if not name:
+        if request.method == 'GET':
+            name = request.args.get('name')
+
+        elif request.method == 'POST':
+            name = request.form['name']
+            if name == '':
+                name = None
+
+    if not name:
+        with open(os.path.join(ROOT, 'sample.dat')) as fp:
+            sample = json.load(fp)
+        name = sample['name']
+
+    return 'Hello ' + name
+
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
